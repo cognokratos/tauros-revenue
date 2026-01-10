73c312f804d420799d5a0e79baaf7b568f62ff4e
Victor Nitu <victor@nitu.lu>
2026-01-10 18:28:46 +0100

    chore: generate authentication
    mix phx.gen.auth Accounts User users

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :tauros, :scopes,
+  user: [
+    default: true,
+    module: Tauros.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: Tauros.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :tauros,
   ecto_repos: [Tauros.Repo],
   generators: [timestamp_type: :utc_datetime, binary_id: true]
